498098b79777afd9517b8fe586c88bdd62eb856d
Olivia Uesugui <uesugui.li@gmail.com>
2022-11-09 01:34:24 -0300

    style: feita uma simples estilização nas páginas não encontrado e não autorizado

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -4,11 +4,13 @@ const NotFound = () => {
   const navigate = useNavigate();
   const goBack = () => navigate(-1);
   return (
-    <section>
-      <h1>Não encontrado</h1>
-      <br />
-      <p>A página requisitada não foi encontrada.</p>
-      <button onClick={goBack}>Voltar</button>
+    <section className="container">
+      <div className="no-access">
+        <h1>Não encontrado</h1>
+        <br />
+        <p>A página requisitada não foi encontrada.</p>
+        <button onClick={goBack}>Voltar</button>
+      </div>
     </section>
   );
 };
--- src/pages/Unauthorized/index.jsx
+++ src/pages/Unauthorized/index.jsx
@@ -1,14 +1,17 @@
+import "./style.css";
 import { useNavigate } from "react-router-dom";
 
 const Unauthorized = () => {
   const navigate = useNavigate();
   const goBack = () => navigate(-1);
   return (
-    <section>
-      <h1>Não autorizado</h1>
-      <br />
-      <p>Você não tem acesso à página requisitada.</p>
-      <button onClick={goBack}>Voltar</button>
+    <section className="container">
+      <div className="no-access">
+        <h1>Não autorizado</h1>
+        <br />
+        <p>Você não tem acesso à página requisitada.</p>
+        <button onClick={goBack}>Voltar</button>
+      </div>
     </section>
   );
 };
