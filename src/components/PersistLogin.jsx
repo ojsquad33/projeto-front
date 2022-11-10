dceede72116e219802fe721da3a85d200e8fa947
Olivia Uesugui <uesugui.li@gmail.com>
2022-11-10 10:11:52 -0300

    fix: alterado o código por conta de erro no persistir login

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -2,16 +2,18 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 // import useRefreshToken from "../hooks/useRefreshToken";
 import useAuth from "../hooks/useAuth";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 
 const Persistlogin = () => {
   const [isLoading, setIsLoading] = useState(true);
   // const refresh = useRefreshToken();
   const { auth } = useAuth();
+  const axiosPrivate = useAxiosPrivate();
 
   useEffect(() => {
     const verifyToken = async () => {
       try {
-        await auth?.accessToken;
+        await axiosPrivate(); //testar se é isso mesmo.
       } catch (error) {
         console.error(error);
       } finally {
